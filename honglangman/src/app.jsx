a49e629bbfe90668cb451b428e42ec0b6a65e0b6
qianzhiwei <380659419@qq.com>
2023-03-04 22:39:28 +0800

    first commit

---
+++ honglangman/src/app.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+    return (
+        <>
+        <h1>Test.com</h1>
+        <Child></Child>
+        </>
+    )
+}
+
+const Child = () => {
+    return (
+        <p>i'm demo child</p>
+    )
+}
+
+export default App;
